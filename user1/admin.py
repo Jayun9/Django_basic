67d6b072aaca82d22834d131e8a8e5e8d6387445
koo <ninejy_lol@naver.com>
2020-10-07 17:37:49 +0900

    회원가입 및 테마 구현

--- user1/admin.py
+++ user1/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import User1
 # Register your models here.
+
+class User1Admin(admin.ModelAdmin):
+    list_display = ('username', 'password')
+
+admin.site.register(User1, User1Admin)
--- user1/models.py
+++ user1/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
 class User1(models.Model):
-    username = models.CharField(max_length=64,
-                                verbose_name='username')
+    username = models.CharField(max_length=32,
+                                verbose_name='사용자명')
+    useremail = models.EmailField(max_length=128,
+                                  default="test@gmail.com",
+                                  verbose_name="사용자 이메일")
     password = models.CharField(max_length=64,
-                                verbose_name='password')
+                                verbose_name='비밀번호')
     registered_dttm = models.DateTimeField(auto_now_add=True,
-                                           verbose_name='time')
-    
+                                           verbose_name='등록시간')
+
+    def __str__(self):
+        return self.username
+
     class Meta:
         db_table = 'basic_user1'
+        verbose_name = "패스트 캠퍼스 사용자"
+        verbose_name_plural = '패스트 캠퍼스 사용자'
